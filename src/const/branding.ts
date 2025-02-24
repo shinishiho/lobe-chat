c5cbcccfaa1c77a806de696dc38e49e0eaf500a6
w <w@NakXKri.local>
2025-02-24 16:58:56 +0700

    Custom branding

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -4,8 +4,10 @@
 
 export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
-export const BRANDING_LOGO_URL = '';
+// export const BRANDING_NAME = 'LobeChat';
+// export const BRANDING_LOGO_URL = '';
+export const BRANDING_NAME = 'IAmW Chat';
+export const BRANDING_LOGO_URL = '/icons/icon-512x512.png';
 
 export const ORG_NAME = 'LobeHub';
 
